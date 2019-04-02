2a4d232d4b9b2a34fad1754efb73cc6a53f6c387
RogerNov <rogerenmanuel08@hotmail.com>
2019-04-02 16:07:27 -0600

    creando vista y rutas

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,8 +8,17 @@ class Question(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('datos publicados')
 
+    def _str_(self):
+        return self.question
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choise(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
+
+    def _str_(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,38 @@
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola mundo")
+    lastes_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question for q in latest_question_list])
+    #return HttpResponse(output)
+   #esto es con el  template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list':lastes_question_list
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html',context)
+
+
+
+   #estos son datos que se mostrar en la vista y seran llamada atraves del patron url 
+def detail(request, question_id):
+    #response = "Estas mirando las preguntas %s."
+    
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("La pregunta no existe!")   
+    return render(request, 'polls/detail.html', {'question':question})
+
+
+
+    
+def results(request, question_id):
+    response = "Estas viendo los resultado de la pregunta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Estas votando en la pregunta %s." % question_id)
